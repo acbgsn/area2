8bb65da169cacfc66577cfa201ae9bfdc44c3459
acbgsn <acbgsn@gmail.com>
2026-05-13 18:21:27 -0300

    Adaptações gerais iniciais da versão 2.0

---
+++ pags/checagem.js
@@ -0,0 +1,8 @@
+var marcado = document.getElementById('marcar');
+if (um.checked) {
+    document.write('UM marcado');
+} else if (dois.checked) {
+    document.write('DOIS marcado');
+} else {
+    document.write('NENHUM marcado');
+}       
